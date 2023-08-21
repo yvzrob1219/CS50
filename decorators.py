291174fe65e0eb5dcb4429b6cdf4af251b9badb5
yvzrob1219 <robbaban12@gmail.com>
2023-08-22 00:20:14 +0800

    first commit

---
+++ decorators.py
@@ -0,0 +1,13 @@
+def annouce(f):
+    def wrapper():
+        print("About to run the function...")
+        f()
+        print("Done with the function")
+    return wrapper
+
+@annouce
+def hello():
+    print("Hello, world!")
+
+
+hello()
